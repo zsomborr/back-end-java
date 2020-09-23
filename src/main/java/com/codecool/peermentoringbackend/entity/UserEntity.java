2d451a65ea8ddba27f5c138809174d56bbdc79c2
Vivien Walter <walter.vivien.k@gmail.com>
2020-09-23 14:34:41 +0200

    add initial user database setup

--- src/main/java/com/codecool/peermentoringbackend/entity/UserEntity.java
+++ src/main/java/com/codecool/peermentoringbackend/entity/UserEntity.java
@@ -1,4 +1,35 @@
 package com.codecool.peermentoringbackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 }
