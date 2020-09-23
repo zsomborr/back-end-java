a146f4fd1e8d476ffda0a51ecfb245a4ec883df4
Vivien Walter <walter.vivien.k@gmail.com>
2020-09-23 13:45:39 +0200

    First commit

---
+++ src/main/java/com/codecool/peermentoringbackend/PeerMentoringBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.peermentoringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerMentoringBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerMentoringBackEndApplication.class, args);
+    }
+
+}
