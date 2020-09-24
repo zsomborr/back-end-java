2479aca68e1d243825b96bb36ed1a7e3668ccf34
Vivien Walter <walter.vivien.k@gmail.com>
2020-09-24 11:26:25 +0200

    add cross origin to reg controller

--- src/main/java/com/codecool/peermentoringbackend/controller/RegistrationController.java
+++ src/main/java/com/codecool/peermentoringbackend/controller/RegistrationController.java
@@ -3,12 +3,10 @@ package com.codecool.peermentoringbackend.controller;
 import com.codecool.peermentoringbackend.model.UserModel;
 import com.codecool.peermentoringbackend.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/reg")
 public class RegistrationController {
 
