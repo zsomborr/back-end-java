58e5569bc320178146926dbd34957b9aa4a54789
sibaluca12 <sibaluca@gmail.com>
2020-10-05 14:10:01 +0200

    deleted unused functions from userrepository

--- src/main/java/com/codecool/peermentoringbackend/repository/UserRepository.java
+++ src/main/java/com/codecool/peermentoringbackend/repository/UserRepository.java
@@ -16,27 +16,14 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByEmail(String email);
 
-
     Optional<UserEntity> findByUsername(String username);
-
 
     UserEntity findDistinctById(Long id);
 
     UserEntity findDistinctByUsername(String username);
 
-
-//    @Query("SELECT DISTINCT u FROM UserEntity u JOIN u.technologyTags t JOIN  u.projectTags p")
-//    List<UserEntity> getIfHasTechAndProjectTags();
-
-    @Query("SELECT DISTINCT u FROM UserEntity u JOIN FETCH u.technologyTags t where t.technologyTag in :techTag")
-    List<UserEntity> getIfHasSpecificTechTag( @Param("techTag") TechnologyEntity techTag);
-
     @Query("SELECT DISTINCT u FROM UserEntity u WHERE u.technologyTags is not empty OR u.projectTags is not empty")
     List<UserEntity> getIfHasProjectOrTechTags();
-
-    @Query("SELECT distinct u FROM UserEntity u  WHERE u.technologyTags in :techTag")
-    List<UserEntity> getMentorByTags( @Param("techTag") TechnologyEntity techTag);
-
 
 
 }
