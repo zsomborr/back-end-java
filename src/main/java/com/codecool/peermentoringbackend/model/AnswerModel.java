e1fa5f3f0427f68f389e24550576e6b438253a72
zsomborr <zsomborkincses@gmail.com>
2021-06-24 16:55:38 +0200

    fix: extended model with builder
    Co-authored-by: mgroholy <mgroholy@users.noreply.github.com>

--- src/main/java/com/codecool/peermentoringbackend/model/AnswerModel.java
+++ src/main/java/com/codecool/peermentoringbackend/model/AnswerModel.java
@@ -2,12 +2,14 @@ package com.codecool.peermentoringbackend.model;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AnswerModel {
 
     private Long userId;
