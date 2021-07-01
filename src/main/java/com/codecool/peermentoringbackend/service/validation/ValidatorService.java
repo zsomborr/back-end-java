7a9df5082b845aaee96575f8ad08bcf11fb1c543
Máté Groholy <mate.groholy@gmail.com>
2021-07-01 10:36:39 +0200

    fix: modify registration validation in RegistrationService and ValidationService. Throw CredentialException on invalid credentials.

--- src/main/java/com/codecool/peermentoringbackend/service/validation/ValidatorService.java
+++ src/main/java/com/codecool/peermentoringbackend/service/validation/ValidatorService.java
@@ -3,6 +3,7 @@ package com.codecool.peermentoringbackend.service.validation;
 import com.codecool.peermentoringbackend.model.UserModel;
 import org.springframework.stereotype.Service;
 
+import javax.security.auth.login.CredentialException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,29 +11,47 @@ import java.util.regex.Pattern;
 public class ValidatorService {
 
 
+    public void validateRegistration(UserModel userModel, int minLengthName, int maxLengthName, int minLengthUsername, int maxLengthUsername) throws CredentialException {
+
+        validateName(userModel.getFirstName(), minLengthName, maxLengthName);
+        validateName(userModel.getLastName(), minLengthName, maxLengthName);
+        validatePassword(userModel.getPassword());
+        validateUsername(userModel.getUsername(), minLengthUsername, maxLengthUsername);
+
+    }
+
     //checks first or last name for length (min and max) and containing characters
-    public boolean validateName(String name, int minLength, int maxLength) {
-        return isAlpha(name) && name.length() >= minLength && name.length() <= maxLength;
+    public void validateName(String name, int minLength, int maxLength) throws CredentialException {
+        if (!(name.length() >= minLength)) {
+            throw new CredentialException(String.format("Name has to be longer than %d characters.", minLength));
+        }
+        if (!(name.length() <= maxLength)){
+            throw new CredentialException(String.format("Name cannot be longer than %d characters.", maxLength));
+        }
+        if (!isAlpha(name)){
+            throw new CredentialException("Name can only contain alphabetic characters.");
+        }
     }
 
     //checks password for min and max length
-    public boolean validatePassword(String password) {
-        return isValidPasswordSecure(password);
+    public void validatePassword(String password) throws CredentialException {
+        if (!isValidPasswordSecure(password)) {
+            throw new CredentialException("Password has to contain upper and lowercase characters, at least one digit and at least one special character (@#$%^&+=)!");
+        }
     }
 
     //checks username for min and max length
-    public boolean validateUsername(String username, int minLength, int maxLength) {
-        return username.length() >= minLength && username.length() <= maxLength && isAlphaNumWithUnderscore(username);
-    }
+    public void validateUsername(String username, int minLength, int maxLength) throws CredentialException {
+        if (!(username.length() >= minLength)) {
+            throw new CredentialException(String.format("Username has to be longer than %d characters.", minLength));
+        }
+        if (!(username.length() <= maxLength)){
+            throw new CredentialException(String.format("Username cannot be longer than %d characters.", maxLength));
+        }
+        if (!isAlphaNumWithUnderscore(username)){
+            throw new CredentialException("Username can only contain alphabetic characters and underscores. Username cannot start or end with underscore.");
+        }
 
-    public boolean validateRegistration(UserModel userModel, int minLengthName, int maxLengthName, int minLengthUsername, int maxLengthUsername) {
-
-        boolean validFirstName = validateName(userModel.getFirstName(), minLengthName, maxLengthName);
-        boolean validLastName = validateName(userModel.getLastName(), minLengthName, maxLengthName);
-        boolean validPassword = validatePassword(userModel.getPassword());
-        boolean validUsername = validateUsername(userModel.getUsername(), minLengthUsername, maxLengthUsername);
-
-        return validFirstName && validLastName && validPassword && validUsername;
     }
 
     private boolean isAlpha(String string) {
@@ -44,8 +63,6 @@ public class ValidatorService {
     }
 
     private boolean isValidPasswordSecure(String password) {
-
-
         return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$");
     }
 }
