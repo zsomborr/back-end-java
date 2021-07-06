496de486f13854e12bdcebca8a36d3dfa9ef388f
Máté Groholy <mate.groholy@gmail.com>
2021-07-06 16:55:13 +0200

    refactor: search method in SearchService returns list of PublicQuestionModels.

--- src/main/java/com/codecool/peermentoringbackend/service/SearchService.java
+++ src/main/java/com/codecool/peermentoringbackend/service/SearchService.java
@@ -1,7 +1,10 @@
 package com.codecool.peermentoringbackend.service;
 
 import com.codecool.peermentoringbackend.entity.QuestionEntity;
+import com.codecool.peermentoringbackend.model.PublicQuestionModel;
+import com.codecool.peermentoringbackend.model.PublicUserModel;
 import com.codecool.peermentoringbackend.repository.QuestionRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +16,26 @@ import java.util.*;
 public class SearchService {
 
     private QuestionRepository questionRepository;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public SearchService(QuestionRepository questionRepository) {
+    public SearchService(QuestionRepository questionRepository, ModelMapper modelMapper) {
         this.questionRepository = questionRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public List<QuestionEntity> search(List<String> words){
-        List<QuestionEntity> questions = new ArrayList<>();
+    public List<PublicQuestionModel> search(List<String> words){
+        List<PublicQuestionModel> questions = new ArrayList<>();
         for (String word : words) {
             Optional<List<QuestionEntity>> distinctByTitleLikeOrDescriptionLike = questionRepository.findDistinctByTitleContainingOrDescriptionContaining(word, word);
             if (distinctByTitleLikeOrDescriptionLike.isPresent()){
                 List<QuestionEntity> questionEntities = distinctByTitleLikeOrDescriptionLike.get();
                 for (QuestionEntity questionEntity : questionEntities) {
-                    questionEntity.setUserData();
-                    questionEntity.setNumberOfAnswers(questionEntity.getAnswers().size());
-                    questions.add(questionEntity);
+                    PublicQuestionModel publicQuestionModel = modelMapper.map(questionEntity, PublicQuestionModel.class);
+                    publicQuestionModel.setUsername(questionEntity.getUser().getUsername());
+                    publicQuestionModel.setUserId(questionEntity.getUser().getId());
+                    publicQuestionModel.setNumberOfAnswers(questionEntity.getAnswers().size());
+                    questions.add(publicQuestionModel);
                 }
             }
         }
