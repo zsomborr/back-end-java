450c3b2e5be02edb9bf8ae08fee192b49562a31e
Máté Groholy <mate.groholy@gmail.com>
2021-07-08 15:22:47 +0200

    feat: create saveNewPassword method in ForgottenPasswordController.

--- src/main/java/com/codecool/peermentoringbackend/controller/ForgottenPasswordController.java
+++ src/main/java/com/codecool/peermentoringbackend/controller/ForgottenPasswordController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.security.auth.login.CredentialException;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -41,6 +42,22 @@ public class ForgottenPasswordController {
             return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/save")
+    public ResponseEntity<ApiResponse> saveNewPassword(@RequestBody Map<String, String> body){
+        ApiResponse apiResponse = new ApiResponse();
+        try{
+            forgottenPasswordService.saveNewPassword(body);
+        } catch (CredentialException | IllegalArgumentException exception) {
+            apiResponse.setSuccess(false);
+            apiResponse.setMessage(exception.getMessage());
+            return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        }
+        apiResponse.setSuccess(true);
+        apiResponse.setMessage("Password saved successfully!");
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+
     }
 
 }
