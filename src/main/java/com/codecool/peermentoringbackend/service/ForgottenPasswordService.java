4694fe911417da1ba4154ad3f2d473214ddd47e2
Máté Groholy <mate.groholy@gmail.com>
2021-07-08 15:50:41 +0200

    feat: modify ForgottenPasswordService. Store email codes in database.

--- src/main/java/com/codecool/peermentoringbackend/service/ForgottenPasswordService.java
+++ src/main/java/com/codecool/peermentoringbackend/service/ForgottenPasswordService.java
@@ -1,6 +1,8 @@
 package com.codecool.peermentoringbackend.service;
 
+import com.codecool.peermentoringbackend.entity.ForgottenPasswordCodeEntity;
 import com.codecool.peermentoringbackend.entity.UserEntity;
+import com.codecool.peermentoringbackend.repository.ForgottenPasswordCodeRepository;
 import com.codecool.peermentoringbackend.repository.UserRepository;
 import com.codecool.peermentoringbackend.service.validation.ValidatorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,52 +19,60 @@ public class ForgottenPasswordService {
     private UserRepository userRepository;
     private EmailSenderService emailSenderService;
     private ValidatorService validatorService;
+    private ForgottenPasswordCodeRepository forgottenPasswordCodeRepository;
     private static Random random = new Random();
-    private Map<String, Integer> randomNumbers;
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public ForgottenPasswordService(UserRepository userRepository, EmailSenderService emailSenderService, ValidatorService validatorService) {
+    public ForgottenPasswordService(UserRepository userRepository, EmailSenderService emailSenderService, ValidatorService validatorService, ForgottenPasswordCodeRepository forgottenPasswordCodeRepository) {
         this.userRepository = userRepository;
         this.emailSenderService = emailSenderService;
         this.validatorService = validatorService;
         this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        this.randomNumbers = new HashMap<>();
+        this.forgottenPasswordCodeRepository = forgottenPasswordCodeRepository;
     }
 
     public boolean forgotPassword(String email){
-        boolean userEmailExists = userRepository.existsByEmail(email);
-        if(!userEmailExists){
+        Optional<UserEntity> userEntityOptional = userRepository.findByEmail(email);
+        if(!userEntityOptional.isPresent()){
             throw new NoSuchElementException("User with email " + email + " not found!");
         }
+        UserEntity userEntity = userEntityOptional.get();
         int min = 100000;
         int max = 999999;
         int randomNumber = random.nextInt(max - min) + min;
-        randomNumbers.put(email, randomNumber);
+        ForgottenPasswordCodeEntity code = ForgottenPasswordCodeEntity.builder().user(userEntity).code(randomNumber).build();
+        forgottenPasswordCodeRepository.save(code);
         String message = String.format("Password forgotten. Use code to set new password. Code: %d", randomNumber);
         emailSenderService.sendMail(email, message, "Forgotten password");
         return true;
     }
 
     public boolean saveNewPassword(Map<String, String> body) throws CredentialException {
-        int code = Integer.parseInt(body.get("code"));
+        int requestCode = Integer.parseInt(body.get("code"));
         String email = body.get("email");
         String password1 = body.get("password1");
         String password2 = body.get("password2");
-        if(code != randomNumbers.get(email)){
+        Optional<UserEntity> userEntityOptional = userRepository.findByEmail(email);
+        if(!userEntityOptional.isPresent()){
+            throw new NoSuchElementException("User with email " + email + " not found!");
+        }
+        UserEntity userEntity = userEntityOptional.get();
+        Optional<ForgottenPasswordCodeEntity> codeOptional = forgottenPasswordCodeRepository.findByUser(userEntity);
+        if(!codeOptional.isPresent()){
+            throw new IllegalArgumentException("Code is not valid");
+        }
+        ForgottenPasswordCodeEntity code = codeOptional.get();
+        if(requestCode != code.getCode()){
             throw new IllegalArgumentException("Code is not valid");
         }
         if(!password1.equals(password2)){
             throw new IllegalArgumentException("Passwords do not match.");
         }
         validatorService.validatePassword(password1);
-        Optional<UserEntity> userEntityOptional = userRepository.findByEmail(email);
-        if(userEntityOptional.isPresent()){
-            UserEntity userEntity = userEntityOptional.get();
-            userEntity.setPassword(passwordEncoder.encode(password1));
-            userRepository.save(userEntity);
-        }
-        randomNumbers.remove(email);
+        userEntity.setPassword(passwordEncoder.encode(password1));
+        userRepository.save(userEntity);
+        forgottenPasswordCodeRepository.delete(code);
         return true;
     }
 }
